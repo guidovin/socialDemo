5bc9597691e61c940cf052e27c2e0c8a31e0e30b
guidovin <guidovin@gmail.com>
2020-09-08 13:50:22 -0300

    changes regex check to not return anything if failed

--- src/dbMock/db.ts
+++ src/dbMock/db.ts
@@ -2,9 +2,14 @@ import users from "./users";
 
 function find(name: string) {
   const checkName = new RegExp("[a-zA-Z]+", "i");
-  if(!name || !(checkName.test(name))) return users;
+  if(!name) 
+    return users;
+  if(name && !checkName.test(name))
+    return [];
   if(name)
-    return users.filter(user => (user.name && user.name.includes(name))) 
+    return users.filter(user => 
+      (user.name && user.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+    )); 
 }
 
 export default { find };
